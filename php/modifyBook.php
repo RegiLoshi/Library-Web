cb834304196d52076303288d700ceaf5f885fbd5
Melis Derveni <melisderveni@yahoo.com>
2024-05-21 01:07:40 +0200

    delete books not Working TT

--- php/modifyBook.php
+++ php/modifyBook.php
@@ -23,13 +23,35 @@ $query = "SELECT * FROM book WHERE ISBN = :ISBN";
 $result = $conn->prepare($query);
 $result->execute([':ISBN' => $ISBN]);
 
+
+$query = "SELECT *
+FROM Book b
+JOIN hasWritten hw ON b.BookId = hw.BookId
+JOIN Author a ON hw.authorId = a.authorId
+WHERE b.ISBN = :ISBN ";
+$result2 = $conn->prepare($query);
+$result2->execute([':ISBN' => $ISBN]);
+
+$query = "SELECT bc.Category
+FROM Book b
+JOIN belongsTo bt ON b.BookId = bt.BookId
+JOIN BookCategory bc ON bt.BookCategoryId = bc.BookCategoryId
+WHERE b.ISBN = :ISBN "; 
+
+$result3 =$conn->prepare($query);
+$result3->execute([':ISBN' => $ISBN]);
+
 $message='';
 $error = '';
-
-///////////////TO BE EDITED////////////////////////
-
 if (isset($_POST['delete'])) {
-
+    $deleteQuery = "DELETE FROM Book
+    WHERE ISBN = :ISBN ";
+    $deleteStatement = $conn->prepare($deleteQuery);
+    $deleteStatement->execute([':ISBN' => $ISBN]);
+    $message = 'Book deleted successfully.';
+    echo $message;
+    header('location: adminBookManage.php');
+    exit();
 }
 
 if (isset($_POST['edit_librarian'])){
@@ -67,9 +89,53 @@ if (isset($_POST['edit_librarian'])){
 
             foreach ($result as $row) {
                 ?>
+                
                 <form method="post">
-                    <!--TO BE EDITED-->
-                    <div class="mt-4 mb-0">
+                    <div class="d-flex">
+                
+                <div class="mb-2">
+                        <label class="form-label">Book Title</label>
+                        <input type="text" name="book_title" id="book_title" class="form-control" style="width:600px"
+                            value="<?php echo $row['title']; ?>"  />
+                </div>
+                <img src="<?php echo $row['bookURL']; ?>" alt="" style="height:70px; width:50px; margin-left:100px;">
+                </div>
+                <div class="mb-2">
+                        <label class="form-label">Book Description</label>
+                        <textarea name="description" id="book_description" class="form-control" style="height:120px;"></textarea> 
+                            
+                </div>
+                <div class="mb-2">
+                        <label class="form-label">Book URL</label>
+                        <input type="url" name="book_url" id="book_url" class="form-control"/>
+                </div>
+                <div class="mb-2">
+                        <label class="form-label">Book Supplier</label>
+                        <input type="text" name="book_supplier" id="book_supplier" class="form-control" 
+                            value="<?php echo $row['supplierName']; ?>"  />
+                </div>
+                <div class="mb-2">
+                        <label class="form-label">Book Quantity</label>
+                        <input type="number" name="book_quantity" id="book_quantity" class="form-control" 
+                            value="<?php echo $row['Quantity']; ?>"  />
+                </div>
+                <div class="mb-2">
+                        <label class="form-label" readonly>ISBN:<?php echo $row['ISBN']; ?> </label>
+                </div>
+                <div class="mb-2">
+                        <?php foreach ($result2 as $row)
+                        {
+                            echo '<label class="form-label" readonly> Authors:  </label>'.$row['firstName'].$row['lastName'].'';
+                        }  ?>
+                </div>
+                <div class="mb-2">
+                        <?php foreach ($result3 as $roww)
+                        {
+                            echo '<label class="form-label" readonly> Categories:  </label>'.$roww['Category'].'';
+                        }  
+                        ?>
+                </div>
+                    <div class="mt-2 mb-0">
                         <input type="submit" name="edit_book" class="btn btn-primary" value="Edit" />
                         <input type="submit" name="delete" class="btn btn-danger" value="Delete" />
                         <input type="hidden" name="book_ISBN" value="<?php echo $row['ISBN']; ?>">
